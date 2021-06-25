48cb9050851d149c97b688764b60a40211f11ae2
Isaac Rabiu <ziktech2020@gmail.com>
2021-06-25 15:35:06 +0100

    Project Updated

--- nathan/settings.py
+++ nathan/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR,'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -123,6 +124,7 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
 django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
